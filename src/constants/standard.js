f014996081c9c92997440debedd52c535302c071
mallah-elmehdi <elmehdimallah@gmail.com>
2024-04-01 17:02:03 +0000

    Add CSS standard file

---
+++ src/constants/standard.js
@@ -0,0 +1,29 @@
+export const BORDER_RADIUS = {
+    none: '0',
+    sm: '0.125rem',
+    base: '0.25rem',
+    md: '0.375rem',
+    lg: '0.5rem',
+    xl: '0.75rem',
+    '2xl': '1rem',
+    '3xl': '1.5rem',
+    full: '9999px',
+};
+
+export const FONTSIZE = {
+    micro: '0.35rem',
+    xxs: '0.5rem',
+    xs: '0.75rem',
+    sm: '0.875rem',
+    md: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.5rem',
+    '3xl': '1.875rem',
+    '4xl': '2.25rem',
+    '5xl': '3rem',
+    '6xl': '3.75rem',
+    '7xl': '4.5rem',
+    '8xl': '6rem',
+    '9xl': '8rem',
+};
